158b8cdf10b60330f84fc46382e5a1070dcfd0e6
PhanXuanThanh031020022 <88616057+PhanXuanThanh031020022@users.noreply.github.com>
2021-09-15 16:19:32 +0700

    up

--- foxic/slider.js
+++ foxic/slider.js
@@ -4,7 +4,31 @@ $('.slider').slick({
     infinite: false,
     slidesToShow: 5,
     slidesToScroll: 1,
-    
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
   
   var filtered = false;
