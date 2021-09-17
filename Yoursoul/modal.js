1713a08cf3df5dd08bd623629b52344b33f58ed7
PhanXuanThanh031020022 <88616057+PhanXuanThanh031020022@users.noreply.github.com>
2021-09-18 01:31:22 +0700

    up

---
+++ Yoursoul/modal.js
@@ -0,0 +1,35 @@
+const showLog=document.querySelector('.js-showlog')
+const modal=document.querySelector('.modal')
+const closeLog=document.querySelector('.js-close')
+const showregister=document.querySelector('.js-showlog-too')
+const modalToo=document.querySelector('.modal-too')
+const closeLogtooo=document.querySelector('.js-close-too')
+const linkchild=document.querySelector('.js-modal-child')
+const linkChildlogin=document.querySelector('.js-show-chile-res')
+//Ham hien
+function ShowLog(){
+    modal.classList.add('open')
+}
+function CloseLog(){
+    modal.classList.remove('open')
+}
+function ShowRegister(){
+    modalToo.classList.add('open')
+}
+function CloseLogToo(){
+    modalToo.classList.remove('open')
+}
+function Showlinkchild(){
+    modalToo.classList.remove('open')
+    modal.classList.add('open')
+}
+function Showlinkchildlogin(){
+    modal.classList.remove('open')
+    modalToo.classList.add('open')
+}
+showregister.addEventListener('click',ShowRegister)
+showLog.addEventListener('click',ShowLog)
+closeLog.addEventListener('click',CloseLog)
+closeLogtooo.addEventListener('click',CloseLogToo)
+linkchild.addEventListener('click',Showlinkchild)
+linkChildlogin.addEventListener('click',Showlinkchildlogin)
