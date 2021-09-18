2f84409618726e774325aeedd15f9ccaef12da36
PhanXuanThanh031020022 <88616057+PhanXuanThanh031020022@users.noreply.github.com>
2021-09-19 00:18:40 +0700

    up

--- Yoursoul/javascript/main.js
+++ Yoursoul/javascript/main.js
@@ -9,5 +9,7 @@ $('.banner').slick({
   $('.slider__col1-fa').slick({
     slidesToShow: 3,
   slidesToScroll: 1,
-  
+  speed:300,
+  prevArrow: '<button class="slick-prev slick-prev-col2" ></button>',
+  nextArrow: '<button class="slick-next slick-next-col2" ></button>',
   });
