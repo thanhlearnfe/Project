cb92d6ee9f801214e0cf0c02d90899b415c8c9d0
PhanXuanThanh031020022 <88616057+PhanXuanThanh031020022@users.noreply.github.com>
2021-09-23 15:44:16 +0700

    up

---
+++ Yoursoul/javascript - Copy/main.js
@@ -0,0 +1,17 @@
+$('.banner').slick({
+  
+    slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  });
+
+
+  $('.slider__col1-fa').slick({
+   
+    slidesToShow: 3,
+  slidesToScroll: 1,
+  speed:300,
+  prevArrow: '<button class="slick-prev slick-prev-col2" ></button>',
+  nextArrow: '<button class="slick-next slick-next-col2" ></button>',
+  });
---
+++ Yoursoul/javascript - Copy/signin.js
@@ -0,0 +1,31 @@
+var inputUsername = document.getElementById('username');
+var inputPassword= document.getElementById('password');
+var CORRECT_USER='phanxuanthanh';
+var CORRECT_PASSWORD ='123456';
+var formLogin = document.getElementById('form-login');
+if (formLogin.attachEvent){
+    formLogin.attachEvent('submit',onFormSubmit);
+}
+else{
+    formLogin.addEventListener('submit',onFormSubmit)
+}
+
+function onFormSubmit(e) {
+    if(e.preventDefault) e.preventDefault();// Ngan hanh dong reload trang
+
+    var username = inputUsername.value;
+    var password = inputPassword.value;
+
+    if(username == CORRECT_USER && password == CORRECT_PASSWORD) {
+        window.location = './indexchild/aquaman.html';// truy cập đên cửa sổ mới
+    } else {
+        var divErrors = document.getElementById('errors');
+        divErrors.innerHTML =   
+        '<div class="alert alert-danger">' +
+        '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+        '<strong>Có lỗi xảy ra:</strong> Tên đăng nhập hoặc mật khẩu không đúng!' +
+        '</div>';
+    }
+
+    return false;
+}
