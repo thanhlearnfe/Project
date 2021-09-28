71eadca4f0f93db26a2faffa1d243bf98af9a2ea
PhanXuanThanh031020022 <88616057+PhanXuanThanh031020022@users.noreply.github.com>
2021-09-28 11:52:34 +0700

    up

--- Yoursoul/javascript/main.js
+++ Yoursoul/javascript/main.js
@@ -14,4 +14,26 @@ $('.banner').slick({
   speed:300,
   prevArrow: '<button class="slick-prev slick-prev-col2" ></button>',
   nextArrow: '<button class="slick-next slick-next-col2" ></button>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   });
