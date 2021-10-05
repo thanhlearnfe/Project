b0ef4523d4223b2f3d654e8ac1ae5c11cb307e76
PhanXuanThanh031020022 <88616057+PhanXuanThanh031020022@users.noreply.github.com>
2021-10-05 13:23:03 +0700

    Update signin.js

--- Yoursoul/javascript/signin.js
+++ Yoursoul/javascript/signin.js
@@ -18,7 +18,24 @@ function onFormSubmit(e) {
 
     if(username == CORRECT_USER && password == CORRECT_PASSWORD) {
         window.location = './vipindex.html';// truy cập đên cửa sổ mới
-    } else {
+    }
+    else if(username == CORRECT_USER && password != CORRECT_PASSWORD){
+        var divErrors = document.getElementById('errors');
+        divErrors.innerHTML =   
+        '<div class="alert alert-danger">' +
+        '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+        '<strong>Có lỗi xảy ra:</strong> Mật khẩu không đúng hoặc chưa nhập mật khẩu!' +
+        '</div>';
+    }
+    else if(username != CORRECT_USER && password == CORRECT_PASSWORD){
+        var divErrors = document.getElementById('errors');
+        divErrors.innerHTML =   
+        '<div class="alert alert-danger">' +
+        '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+        '<strong>Có lỗi xảy ra:</strong> Tài khoản không đúng hoặc chưa nhập tài khoản!' +
+        '</div>';
+    }
+    else {
         var divErrors = document.getElementById('errors');
         divErrors.innerHTML =   
         '<div class="alert alert-danger">' +
