99f7b05f4590cd68e76cffc526edbdd1281bfae3
thanhlearnfe <xuanthanhvdk@gmail.com>
2022-02-14 10:29:30 +0700

    up

---
+++ zingmp3/script/slideExplore.js
@@ -0,0 +1,71 @@
+/* $('.over-lists-slider2.radio').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 1224,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          infinite: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+ */
+  $('.over-list-container.over-lyrics2.singer').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 1224,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          infinite: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
